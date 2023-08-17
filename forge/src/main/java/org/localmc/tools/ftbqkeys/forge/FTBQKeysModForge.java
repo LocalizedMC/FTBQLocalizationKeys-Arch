8384295c26238082f926a867e5b55b1772cc7cbc
TexTrue <3140846162@qq.com>
2023-08-17 19:26:47 +0800

    fix

--- forge/src/main/java/org/localmc/tools/ftbqkeys/forge/FTBQKeysModForge.java
+++ forge/src/main/java/org/localmc/tools/ftbqkeys/forge/FTBQKeysModForge.java
@@ -1,6 +1,8 @@
 package org.localmc.tools.ftbqkeys.forge;
 
 import me.shedaniel.architectury.platform.forge.EventBuses;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import org.localmc.tools.ftbqkeys.FTBQKeysMod;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -10,6 +12,13 @@ public class FTBQKeysModForge {
     public FTBQKeysModForge() {
         // Submit our event bus to let architectury register our content on the right time
         EventBuses.registerModEventBus(FTBQKeysMod.MODID, FMLJavaModLoadingContext.get().getModEventBus());
+
+        IEventBus modEventBus = EventBuses.getModEventBus(FTBQKeysMod.MODID).get();
+
+        modEventBus.addListener(this::onInitialize);
+    }
+
+    public void onInitialize(FMLCommonSetupEvent event) {
         FTBQKeysMod.init();
     }
 }
